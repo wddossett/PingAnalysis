fbb416848a3fbbd0a3cfbe58ae703c79fad62954
bi003do <bi003do@USBOU-BI003DOPC>
2017-09-01 13:38:26 -0600

    Add project files.

---
+++ pingAnalysis/pingAnalysis.py
@@ -0,0 +1,19 @@
+
+import sys
+import os
+import pingparsing
+import re
+
+
+regex = r"^(.{3}) ([0-9]{2}\/[0-9]{2}\/[0-9]{4}) ([0-9]{2}:[0-9]{2}:[0-9]{2}).+?time.([0-9])+"
+with open("c:\\users\\bi003do\\log_usbou1-netvault.log") as f:
+    for x in range(0,2):
+        next(f)
+    for line in f:
+         print(line)
+         matches = re.findall(regex, line)
+         if matches: 
+             print(matches[0][0] + "\n")
+             print(matches[0][1] + "\n")
+             print(matches[0][2] + "\n")
+             print(matches[0][3] + "\n")
